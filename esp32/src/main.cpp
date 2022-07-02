cf527e0791c70b2662a5c5c044bef81e5944f431
AL Reasat Rafio <rafioar96@gmail.com>
2022-07-02 16:42:51 +0600

    :sparkles: esp32 wifi connection added

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -1,9 +1,24 @@
 #include <Arduino.h>
+#include <WiFi.h>
+
+void initWifi() {
+    const char *ssid = "Imposter";
+    const char *password = "rrrrrrrr";
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    Serial.print("Connecting to WiFi ..");
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print('.');
+        delay(1000);
+    };
+    Serial.println(WiFi.localIP());
+};
 
 void setup() {
-  // put your setup code here, to run once:
+    Serial.begin(115200);
+    initWifi();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    delay(200);
 }
